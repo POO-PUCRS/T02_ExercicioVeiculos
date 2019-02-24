f62fcc710028ad840f73db43510dd7096bbeb65e
Bernardo Copstein <bernardo.copstein@pucrs.br>
2019-02-24 08:04:04 -0300

    first commit

---
+++ App.java
@@ -0,0 +1,13 @@
+public class App{
+    public static void main(String args[]){
+        Placa p1 = new Placa();
+        p1.setPais("Brasil");
+        p1.setCodigo("ABC1R23");
+
+        Veiculo v = new Veiculo(p1);
+        v.abastece(50);
+        v.dirige(250);
+        System.out.println("Dados do veiculo:");
+        System.out.println(v);
+    }
+}
